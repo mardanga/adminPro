87be13ba7898a0ad71d29cf414123102169766a2
mardanga <gaitan@CESVIARG>
2018-05-11 10:27:18 -0300

    primer commit register

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { APP_ROUTES } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HomeComponent } from './pages/home.component';
+import { RegisterComponent } from './login/register.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NotpagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    HomeComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTES
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
